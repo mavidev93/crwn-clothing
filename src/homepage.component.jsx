548aa8fb3d6d4c1f71ef68210459c518c311668e
mavi <mavidev93@gmail.com>
2020-01-06 19:08:27 +0330

    created project boiler plate

---
+++ src/homepage.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './homepage.styls.scss'
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>JACKET</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>SNIKERS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>WOMENS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>MANS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+)
+
+export default HomePage
